b20eaf3f516f0011248ff58471224c9ec2127614
Unknown <cornetistedu29@hotmail.fr>
2020-02-11 10:53:37 +0100

    Test

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import {  Container, Row, Col } from 'react-bootstrap';
+import {  Container, Row, Col, Button } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Websocket from 'react-websocket';
 
 function App() {
   return (
@@ -12,15 +13,14 @@ function App() {
       <header className="App-header">
         <Container>
           <Row>
-            <Col>
+            <Col id="chapters">
             <p>Menu</p>
             </Col>
             <Col>
             <p>Movie</p>
             </Col>
             <Col>
-            <p> Messagerie
-            </p>
+            <p> Messagerie</p>
             </Col>
           </Row>
         </Container>
